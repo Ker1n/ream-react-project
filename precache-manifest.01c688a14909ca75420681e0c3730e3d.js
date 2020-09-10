13900cec4012187825c610eedb5ad6110c7f6f87
Markov Kyrylo <Ker1n.mkc@gmail.com>
2020-09-10 18:02:38 +0300

    Updates

---
+++ precache-manifest.01c688a14909ca75420681e0c3730e3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061a1ff98888be0f204435fbec775a24",
+    "url": "/ream-react-project/index.html"
+  },
+  {
+    "revision": "1612a11073496d623483",
+    "url": "/ream-react-project/static/css/main.0e10cc56.chunk.css"
+  },
+  {
+    "revision": "73ae686c4880abd4e7c9",
+    "url": "/ream-react-project/static/js/2.7917b15f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ream-react-project/static/js/2.7917b15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1612a11073496d623483",
+    "url": "/ream-react-project/static/js/main.dec62100.chunk.js"
+  },
+  {
+    "revision": "6555e1a66bfd004b9a74",
+    "url": "/ream-react-project/static/js/runtime-main.4096bd46.js"
+  },
+  {
+    "revision": "c49b01508c5bf730f578c8c1c407462c",
+    "url": "/ream-react-project/static/media/user-login-icon-14.c49b0150.png"
+  }
+]);
